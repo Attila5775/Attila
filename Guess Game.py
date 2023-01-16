51c6ba9b35323f0d6cbf11997fc793ad5c088ae6
Attila5775 <96767550+Attila5775@users.noreply.github.com>
2023-01-16 13:19:37 +0100

    Update Guess Game.py

--- Guess Game.py
+++ Guess Game.py
@@ -5,18 +5,18 @@ name = input("Enter your name: ")
 print('')
 print(f"Dear {name},\nThis is a game, where you have to guess a number between 1 - 5. You have three oportunities. Good luck!")
 number = random.randint(1, 5)
-number2 = int(input("Enter a number between 1 - 5: "))
+number2 = str(input("Enter a number between 1 - 5: "))
 tries = 2
 
 while number != number2:
     print(f"Number {number2} was incorrect, still you have {tries} tries")
     tries -= 1
-    number2 = int(input("Enter a number between 1 - 5: "))
+    number2 = str(input("Enter a number between 1 - 5: "))
 
     if tries == 1 :
         print(f"Number {number2} was incorrect, still you have {tries} try")
         tries -= 1
-        number2 = int(input("Enter a number between 1 - 5: "))
+        number2 = str(input("Enter a number between 1 - 5: "))
 
     if tries == 0 :
         print("Game over")
