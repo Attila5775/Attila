89ff01bbc45e260452e7a31311a8bb21c1c61cbe
Attila5775 <96767550+Attila5775@users.noreply.github.com>
2023-01-17 14:59:01 +0100

    Update beginner _ Guess Game.py

--- beginner _ Guess Game.py
+++ beginner _ Guess Game.py
@@ -4,13 +4,14 @@ print("="*50, "Guess Game", "="*50)
 name = input("Enter your name: ")
 print('')
 print(f"Dear {name},\nThis is a game, where you have to guess a number between 1 - 5. You have three oportunities. Good luck!")
+
 number = random.randint(1, 6)
 number2 = int(input("Enter a number between 1 - 6: "))
-tries = 2
+tries = 3
 
 while number != number2:
-    print(f"Number {number2} was incorrect, still you have {tries} tries")
     tries -= 1
+    print(f"Number {number2} was incorrect, still you have {tries} tries")
     number2 = int(input("Enter a number between 1 - 6: "))
 
     if tries == 1 :
@@ -21,6 +22,8 @@ while number != number2:
     if tries == 0 :
         print("Game over")
         print(f"The correct number was: {number}")
+        print("="*50, "Guess Game", "="*50)
         exit()
 
 print("Congrat you win!\n" f"My number was {number} too.")
+print("="*50, "Guess Game", "="*50)
