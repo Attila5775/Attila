a2d520f2c4292e0b8c26626f76a6fd34b2e888f0
Attila5775 <96767550+Attila5775@users.noreply.github.com>
2023-01-26 15:37:51 +0100

    Update beginner _ Guess Game.py

--- beginner _ Guess Game.py
+++ beginner _ Guess Game.py
@@ -1,28 +1,52 @@
-import random
+import os, time, random
 
-print("="*50, "Guess Game", "="*50)
-name = input("Enter your name: ")
-print('')
-print(f"Dear {name},\nThis is a game, where you have to guess a number between 1 - 6. You have three oportunities. Good luck!")
+GAMER = input("Enter your name: ")
+TRIES = 3
+MACHINE_NUMBER = random.randint(1, 6)
 
-number = random.randint(1, 6)
-number2 = int(input("Enter a number between 1 - 6: "))
-tries = 3
+def main():
+    clear_screen()
+    print(f"Dear {GAMER}, \nWelcome in the GUESS GAME! :)")
+    time.sleep(3)
+    clear_screen()
+    intro()
+    game()
+    exit()
 
-while number != number2:
-    tries -= 1
-    if tries == 0 :
-        print("Game over")
-        print(f"The correct number was: {number}")
+def intro():
+    print("="*50, "Guess Game", "="*50)
+    print(f"You have to guess a number between 1-6. You will have {TRIES} tries.")
+    time.sleep(5)
+
+def game():
+    user_guess = input("Enter your guess: ")
+    while MACHINE_NUMBER != str(user_guess):
+        TRIES -= 1
+        print(f"Your guess was incorrect, please try it again. You still have {TRIES} tries.")
+        user_guess = input("Enter your guess: ")
+        if TRIES == 1:
+            print(f"The number was: {MACHINE_NUMBER}")
+            print("Game over! You lost.")
+            print("="*50, "Guess Game", "="*50)
+            new_game()
+
+    if MACHINE_NUMBER == str(user_guess):
+        print("You won! Congratulation!")
         print("="*50, "Guess Game", "="*50)
+
+def clear_screen():
+    os.system('cls')
+
+def new_game():
+    new_game = input("Would you like it again: y/n: ")
+    if new_game == "y":
+        game()
+    else:
         exit()
 
-    tries_text = "tries"
-    if tries == 1:
-        tries_text = "try" 
+main()
 
-    print(f"Number {number2} was incorrect, still you have {tries} {tries_text}")
-    number2 = int(input("Enter a number between 1 - 6: "))
 
-print("Congrat you win!\n" f"My number was {number} too.")
-print("="*50, "Guess Game", "="*50)
+
+
+
